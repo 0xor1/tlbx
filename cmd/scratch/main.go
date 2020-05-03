a15b287f8e78fa0f1f31fba4d1b6c4bfd54000f5
Daniel Robinson <daniel.robinson.open@gmail.com>
2020-05-03 21:12:44 +0100

    use NewGame func

--- cmd/scratch/main.go
+++ cmd/scratch/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 
-	"github.com/0xor1/wtf/cmd/boring/pkg/blockers"
+	"github.com/0xor1/wtf/cmd/boring/pkg/blockers/blockerseps"
 	. "github.com/0xor1/wtf/pkg/core"
 	"github.com/0xor1/wtf/pkg/web/app"
 )
@@ -13,67 +13,7 @@ func main() {
 }
 
 func testJsonSizeOfBlockersGame() {
-	g1 := blockers.Game{
-		ID:        app.ExampleID(),
-		CreatedOn: app.ExampleTime(),
-		UpdatedOn: app.ExampleTime(),
-		Started:   false,
-		Players:   []ID{app.ExampleID(), app.ExampleID(), app.ExampleID(), app.ExampleID()},
-		PieceSets: initPieceSets(),
-		TurnIdx:   0,
-		Board:     initBoard(),
-	}
+	g1 := blockerseps.NewGame(app.ExampleID(), app.ExampleID())
 	bs, err := json.Marshal(g1)
 	Println(string(bs), len(bs), err)
-}
-
-func initPieceSets() Bits {
-	return Bits{
-		1, 1, 1, 1, // piece 0  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 1  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 2  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 3  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 4  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 5  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 6  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 7  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 8  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 9  -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 10 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 11 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 12 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 13 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 14 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 15 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 16 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 17 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 18 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 19 -> color 0, 1, 2, 3
-		1, 1, 1, 1, // piece 20 -> color 0, 1, 2, 3
-	}
-}
-
-func initBoard() blockers.Pbits {
-	return blockers.Pbits{
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-	}
 }
