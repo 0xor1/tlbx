9fd34ff0d1b26aeee9e16b996a62c4ea4fe4daa1
Daniel Robinson <daniel.robinson.open@gmail.com>
2020-11-15 10:30:25 +0000

    add currency symbols for major currencies

--- src/fmt/index.js
+++ src/fmt/index.js
@@ -1,7 +1,7 @@
 export default {
-    install(vue, opts){
+    install(vue){
         vue.prototype.$fmt = {
-            duration: (minutes, hoursPerDay, daysPerWeek)=>{
+            duration: (minutes, hoursPerDay, daysPerWeek) => {
                 // hoursPerDay and daysPerWeek are optional, if they arent passed
                 // or are passed as zero just show hours and minutes
                 let h = Math.floor(minutes / 60)
@@ -21,7 +21,42 @@ export default {
                 d = d % daysPerWeek
                 return w + "w " + d + "d " + h + "h " + m + "m"
             },
-            cost: (currencyCode, value) => currencyCode + " " + (value/100).toFixed(2)
+            cost: (currencyCode, value) => {
+                let symbol = currencyCode
+                // only support symbols for the major currencies
+                switch(currencyCode) {
+                    case "USD":
+                        symbol= '$'
+                        break;
+                    case "EUR":
+                        symbol= '€'
+                        break;
+                    case "CAD":
+                        symbol= 'C$'
+                        break;
+                    case "AUD":
+                        symbol= 'A$'
+                        break;
+                    case "JPY":
+                        symbol= '¥'
+                        break;
+                    case "GBP":
+                        symbol= '£'
+                        break;
+                    case "CNY", "CNH":
+                        symbol= 'CN¥'
+                        break;
+                    case "CHF":
+                        symbol= 'Fr'
+                        break;
+                    case "NZD":
+                        symbol= 'NZ$'
+                        break;
+                    default:
+                        symbol = currencyCode + " "
+                }
+                return symbol + (value/100).toFixed(2)
+            }
         }
     }
 }
