a3c8fa8c1a20434bbd5e172d5257b0643922f83b
Daniel Robinson <daniel.robinson.open@gmail.com>
2020-12-23 12:06:44 +0000

    point to new server

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
       port: 8081,
       proxy: {
         '/api/': {
-          target: 'http://localhost:8080',
+          target: 'https://task-trees.com',
           ws: true,
           changeOrigin: true
         }
