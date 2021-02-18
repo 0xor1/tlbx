3b33b6d3d3538a186bc065b721eec356bbbeb29c
Daniel Robinson <daniel.robinson.open@gmail.com>
2021-02-18 17:17:36 +0000

    add collapsible sections

--- cmd/trees/client/src/views/apiDocs.vue
+++ cmd/trees/client/src/views/apiDocs.vue
@@ -7,22 +7,29 @@
       http methods, <strong>args</strong> can be passed as <strong>JSON</strong> in the request body or
       as stringified json in the query parameter args e.g. <strong>?args={"name":"val"}</strong>
     </p>
-    <div class="ep" v-for="(ep, idx) in docs.endpoints" :key="idx">
-      <h3>{{ep.path}}</h3>
-      <p>{{ep.description}}</p>
-      <p>max body size: {{ep.maxBodyBytes === 1000? '1KB': $u.fmt.bytes(ep.maxBodyBytes)}}</p>
-      <p>timeout: {{ep.timeoutMilli}}ms</p>
-      <div>
-        <h4>default args</h4>
-        <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.defaultArgs, null, 4)+'\n```')"></div>
-      </div>
-      <div>
-        <h4>example args</h4>
-        <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.exampleArgs, null, 4)+'\n```')"></div>
-      </div>
-      <div>
-        <h4>example response</h4>
-        <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.exampleResponse, null, 4)+'\n```')"></div>
+    <div v-for="(sec, idx) in docs.sections" :key="idx">
+      <h2 class="expandable" @click.stop.prevent="sec.collapse = !sec.collapse">{{sec.name}} [{{sec.collapse?'+':'-'}}]</h2>
+      <div v-if="!sec.collapse">
+        <div v-for="(ep, idx) in sec.endpoints" :key="idx">
+          <h3 class="expandable" @click.stop.prevent="ep.collapse = !ep.collapse">{{ep.path}} [{{ep.collapse?'+':'-'}}]</h3>
+          <div v-if="!ep.collapse">
+            <p>{{ep.description}}</p>
+            <p>max body size: {{ep.maxBodyBytes === 1000? '1KB': $u.fmt.bytes(ep.maxBodyBytes)}}</p>
+            <p>timeout: {{ep.timeoutMilli}}ms</p>
+            <div>
+              <h4>default args</h4>
+              <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.defaultArgs, null, 4)+'\n```')"></div>
+            </div>
+            <div>
+              <h4>example args</h4>
+              <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.exampleArgs, null, 4)+'\n```')"></div>
+            </div>
+            <div>
+              <h4>example response</h4>
+              <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.exampleResponse, null, 4)+'\n```')"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -43,7 +50,24 @@
       init() {
         this.$u.copyProps(this.initState(), this)
         this.$api.docs().then((docs)=>{
-            this.docs = docs
+          let sectionsArray = []
+          let sectionsMap = {}
+          docs.endpoints.forEach((ep)=>{
+            let rawSegs = ep.path.replace('/api/', '').split('/')
+            if (rawSegs.length > 1) {
+              ep.collapse = true
+              let section = {name: rawSegs[0], collapse: true, endpoints: []}
+              if (sectionsMap[rawSegs[0]] == null) {
+                sectionsMap[rawSegs[0]] = section
+                sectionsArray.push(section)
+              } else {
+                section = sectionsMap[rawSegs[0]]
+              }
+              section.endpoints.push(ep)
+            }
+          })
+          docs.sections = sectionsArray
+          this.docs = docs
         })
       }
     },
@@ -61,7 +85,8 @@
 <style lang="scss" scoped>
 @import "../style.scss";
 
-.ep{
-  border-top: 1px solid $color;
+.expandable{
+  cursor: pointer;
+  text-decoration: underline;
 }
 </style>
