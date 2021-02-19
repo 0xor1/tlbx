c11c0969bf9bd15110b516ec45388dc38cf19c43
Daniel Robinson <daniel.robinson.open@gmail.com>
2021-02-19 12:25:00 +0000

    more api docs tweaks

--- cmd/trees/client/src/views/apiDocs.vue
+++ cmd/trees/client/src/views/apiDocs.vue
@@ -13,28 +13,26 @@
         <div v-for="(ep, epIdx) in sec.endpoints" :key="epIdx">
           <h3 class="expandable" @click.stop.prevent="toggleEp(idx, epIdx)">{{ep.path}} [{{ep.collapse?'+':'-'}}]</h3>
           <div v-if="!ep.collapse">
-            <p>{{ep.description}}</p>
-            <p>max body size: {{ep.maxBodyBytes === 1000? '1KB': $u.fmt.bytes(ep.maxBodyBytes)}}</p>
-            <p>timeout: {{ep.timeout}}ms</p>
-            <div>
-              <h4>args types</h4>
-              <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.argsTypes, null, 4)+'\n```')"></div>
+            <p>{{ep.description}}<br>max body size: {{ep.maxBodyBytes === 1000? '1KB': $u.fmt.bytes(ep.maxBodyBytes)}}<br>timeout: {{ep.timeout}}ms</p>
+            <div v-if="ep.argsTypes != null">
+              <h4 class="expandable" @click.stop.prevent="ep.collapseArgsTypes = !ep.collapseArgsTypes">args types [{{ep.collapseArgsTypes?'+':'-'}}]</h4>
+              <div v-if="!ep.collapseArgsTypes" v-html="$u.fmt.md('```\n'+JSON.stringify(ep.argsTypes, null, 4)+'\n```')"></div>
             </div>
-            <div>
-              <h4>res types</h4>
-              <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.resTypes, null, 4)+'\n```')"></div>
+            <div v-if="ep.resTypes != null">
+              <h4 class="expandable" @click.stop.prevent="ep.collapseResTypes = !ep.collapseResTypes">res types [{{ep.collapseResTypes?'+':'-'}}]</h4>
+              <div v-if="!ep.collapseResTypes" v-html="$u.fmt.md('```\n'+JSON.stringify(ep.resTypes, null, 4)+'\n```')"></div>
             </div>
-            <div>
-              <h4>default args</h4>
-              <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.defaultArgs, null, 4)+'\n```')"></div>
+            <div v-if="ep.defaultArgs != null">
+              <h4 class="expandable" @click.stop.prevent="ep.collapseDefaultArgs = !ep.collapseDefaultArgs">default args [{{ep.collapseDefaultArgs?'+':'-'}}]</h4>
+              <div v-if="!ep.collapseDefaultArgs" v-html="$u.fmt.md('```\n'+JSON.stringify(ep.defaultArgs, null, 4)+'\n```')"></div>
             </div>
-            <div>
-              <h4>example args</h4>
-              <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.exampleArgs, null, 4)+'\n```')"></div>
+            <div v-if="ep.exampleArgs != null">
+              <h4 class="expandable" @click.stop.prevent="ep.collapseExampleArgs = !ep.collapseExampleArgs">example args [{{ep.collapseExampleArgs?'+':'-'}}]</h4>
+              <div v-if="!ep.collapseExampleArgs" v-html="$u.fmt.md('```\n'+JSON.stringify(ep.exampleArgs, null, 4)+'\n```')"></div>
             </div>
-            <div>
-              <h4>example response</h4>
-              <div v-html="$u.fmt.md('```\n'+JSON.stringify(ep.exampleRes, null, 4)+'\n```')"></div>
+            <div v-if="ep.exampleRes != null">
+              <h4 class="expandable" @click.stop.prevent="ep.collapseExampleRes = !ep.collapseExampleRes">example response [{{ep.collapseExampleRes?'+':'-'}}]</h4>
+              <div v-if="!ep.collapseExampleRes" v-html="$u.fmt.md('```\n'+JSON.stringify(ep.exampleRes, null, 4)+'\n```')"></div>
             </div>
           </div>
         </div>
@@ -64,6 +62,11 @@
             let rawSegs = ep.path.replace('/api/', '').split('/')
             if (rawSegs.length > 1) {
               ep.collapse = true
+              ep.collapseArgsTypes = true
+              ep.collapseResTypes = true
+              ep.collapseDefaultArgs = true
+              ep.collapseExampleArgs = true
+              ep.collapseExampleRes = true
               let section = {name: rawSegs[0], collapse: true, endpoints: []}
               if (sectionsMap[rawSegs[0]] == null) {
                 sectionsMap[rawSegs[0]] = section
@@ -87,12 +90,27 @@
           s.collapse = true
           s.endpoints.forEach((ep)=>{
             ep.collapse = true
+            ep.collapseArgsTypes = true
+            ep.collapseResTypes = true
+            ep.collapseDefaultArgs = true
+            ep.collapseExampleArgs = true
+            ep.collapseExampleRes = true
           })
         }
       },
       toggleEp(idx, epIdx) {
         let ep = this.docs.sections[idx].endpoints[epIdx]
-        ep.collapse = !ep.collapse
+         if (ep.collapse) {
+          ep.collapse = false
+        } else {
+          // if collapsing ep, collapse all children for next opening
+          ep.collapse = true
+          ep.collapseArgsTypes = true
+          ep.collapseResTypes = true
+          ep.collapseDefaultArgs = true
+          ep.collapseExampleArgs = true
+          ep.collapseExampleRes = true
+        }
       }
     },
     mounted(){
