36d5f63fa76f7545e7d25f21c12cb5983c551e23
Daniel Robinson <daniel.robinson.open@gmail.com>
2021-09-03 11:05:13 +0100

    fix test coverage, dont show main.go, and dont use separate sub test pkgs anymore for api eps

--- cmd/trees/pkg/task/taskeps/taskeps_test.go
+++ cmd/trees/pkg/task/taskeps/taskeps_test.go
@@ -1,11 +1,433 @@
 package taskeps_test
 
 import (
+	"bytes"
+	"io/ioutil"
 	"testing"
 
-	test "github.com/0xor1/tlbx/cmd/trees/pkg/task/tasktest"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/cnsts"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/config"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/file"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/file/fileeps"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/project"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/project/projecteps"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/task"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/task/taskeps"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/testutil"
+	. "github.com/0xor1/tlbx/pkg/core"
+	"github.com/0xor1/tlbx/pkg/field"
+	"github.com/0xor1/tlbx/pkg/ptr"
+	"github.com/0xor1/tlbx/pkg/web/app/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEverything(t *testing.T) {
-	test.Everything(t)
+	var (
+		tree task.GetTreeRes
+		pID  ID
+	)
+	defer func() {
+		testutil.PrintFullTree(pID, tree)
+	}()
+
+	a := assert.New(t)
+	r := test.NewMeRig(
+		config.Get(),
+		append(append(projecteps.Eps, taskeps.Eps...), fileeps.Eps...),
+		nil,
+		nil,
+		nil,
+		projecteps.OnDelete,
+		projecteps.OnSetSocials,
+		projecteps.ValidateFCMTopic,
+		true,
+		cnsts.FileBucket)
+	defer r.CleanUp()
+
+	ac := r.Ali().Client()
+
+	p := (&project.Create{
+		Name: "0",
+	}).MustDo(ac)
+	a.NotNil(p)
+
+	defer func() {
+		(&project.Delete{p.ID}).MustDo(ac)
+	}()
+
+	(&project.AddUsers{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Users: []*project.SendUser{
+			{
+				ID:   r.Bob().ID(),
+				Role: cnsts.RoleWriter,
+			},
+		},
+	}).MustDo(ac)
+
+	createRes := (&task.Create{
+		Host:       r.Ali().ID(),
+		Project:    p.ID,
+		Parent:     p.ID,
+		PrevSib:    nil,
+		Name:       "1.0",
+		IsParallel: true,
+		User:       ptr.ID(r.Bob().ID()),
+		TimeEst:    100,
+	}).MustDo(ac)
+	a.NotNil(createRes)
+	a.True(p.ID.Equal(createRes.Parent.ID))
+
+	t1p0 := createRes.Task
+
+	t1p1 := (&task.Create{
+		Host:        r.Ali().ID(),
+		Project:     p.ID,
+		Parent:      p.ID,
+		PrevSib:     ptr.ID(t1p0.ID),
+		Name:        "1.1",
+		Description: "1.1",
+		IsParallel:  true,
+		User:        ptr.ID(r.Ali().ID()),
+		TimeEst:     100,
+	}).MustDo(ac).Task
+	a.NotNil(createRes)
+
+	// move first child to a different position
+	t1p0 = (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t1p0.ID,
+		PrevSib: &field.IDPtr{V: &t1p1.ID},
+	}).MustDo(ac).Task
+
+	t2p0 := (&task.Create{
+		Host:        r.Ali().ID(),
+		Project:     p.ID,
+		Parent:      t1p0.ID,
+		PrevSib:     nil,
+		Name:        "2.0",
+		Description: "",
+		IsParallel:  true,
+		User:        ptr.ID(r.Ali().ID()),
+		TimeEst:     100,
+	}).MustDo(ac).Task
+	a.NotNil(t2p0)
+
+	t3p0 := (&task.Create{
+		Host:        r.Ali().ID(),
+		Project:     p.ID,
+		Parent:      t2p0.ID,
+		PrevSib:     nil,
+		Name:        "3.0",
+		Description: "",
+		IsParallel:  true,
+		User:        ptr.ID(r.Ali().ID()),
+		TimeEst:     100,
+	}).MustDo(ac).Task
+	a.NotNil(t3p0)
+
+	t4p0 := (&task.Create{
+		Host:        r.Ali().ID(),
+		Project:     p.ID,
+		Parent:      t1p1.ID,
+		PrevSib:     nil,
+		Name:        "4.0",
+		Description: "",
+		IsParallel:  true,
+		User:        ptr.ID(r.Ali().ID()),
+		TimeEst:     100,
+	}).MustDo(ac).Task
+	a.NotNil(t4p0)
+
+	t1p2 := (&task.Create{
+		Host:        r.Ali().ID(),
+		Project:     p.ID,
+		Parent:      p.ID,
+		PrevSib:     ptr.ID(t1p0.ID),
+		Name:        "1.2",
+		Description: "",
+		IsParallel:  true,
+		User:        ptr.ID(r.Ali().ID()),
+		TimeEst:     50,
+	}).MustDo(ac).Task
+	a.NotNil(t1p2)
+
+	tree = (&task.GetTree{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      p.ID,
+	}).MustDo(ac)
+	a.Len(tree, 7)
+
+	uRes := (&task.Update{
+		Host:        r.Ali().ID(),
+		Project:     p.ID,
+		ID:          t1p1.ID,
+		Parent:      &field.ID{V: t2p0.ID},
+		PrevSib:     nil,
+		Name:        &field.String{V: "1.1 - updated"},
+		Description: &field.String{V: "an actual description"},
+		IsParallel:  &field.Bool{V: false},
+		User:        &field.IDPtr{V: nil},
+		TimeEst:     &field.UInt64{V: 50},
+		CostEst:     &field.UInt64{V: 50},
+	}).MustDo(ac)
+	a.NotNil(uRes)
+	a.True(t1p1.ID.Equal(uRes.Task.ID))
+	a.True(p.ID.Equal(uRes.OldParent.ID))
+	a.True(t2p0.ID.Equal(uRes.NewParent.ID))
+
+	tp := (&task.Update{
+		Host:       r.Ali().ID(),
+		Project:    p.ID,
+		ID:         p.ID,
+		IsParallel: &field.Bool{V: true},
+		TimeEst:    &field.UInt64{V: 50},
+		CostEst:    &field.UInt64{V: 50},
+	}).MustDo(ac)
+	a.NotNil(tp)
+
+	tNil := (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      p.ID,
+	}).MustDo(ac)
+	a.Nil(tNil)
+
+	// try to move 2.0 to be a child of itself
+	tNil, err := (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t2p0.ID,
+		Parent:  &field.ID{V: t2p0.ID},
+	}).Do(ac)
+	a.Nil(tNil)
+	a.Error(err, "ancestor loop detected, invalid parent value")
+
+	// try to move 2.0 to be a descendant of itself
+	tNil, err = (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t2p0.ID,
+		Parent:  &field.ID{V: t4p0.ID},
+	}).Do(ac)
+	a.Nil(tNil)
+	a.Error(err, "ancestor loop detected, invalid parent value")
+
+	t4p0 = (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t4p0.ID,
+		Parent:  &field.ID{V: t2p0.ID},
+		PrevSib: &field.IDPtr{V: ptr.ID(t1p1.ID)},
+	}).MustDo(ac).Task
+	a.NotNil(t4p0)
+
+	// create new sub tree to test move special case
+	t5p0 := (&task.Create{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Parent:  t4p0.ID,
+		Name:    "5.0",
+	}).MustDo(ac).Task
+
+	t6p0 := (&task.Create{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Parent:  t5p0.ID,
+		Name:    "6.0",
+	}).MustDo(ac).Task
+
+	t6p1 := (&task.Create{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Parent:  t5p0.ID,
+		PrevSib: ptr.ID(t6p0.ID),
+		Name:    "6.1",
+	}).MustDo(ac).Task
+
+	//now move 6.1 under 6.0
+	uRes = (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t6p1.ID,
+		Parent:  &field.ID{V: t6p0.ID},
+	}).MustDo(ac)
+	a.True(uRes.OldParent.ID.Equal(t5p0.ID))
+	a.True(uRes.NewParent.ID.Equal(t6p0.ID))
+	a.True(uRes.NewParent.NextSib == nil)
+	a.True(uRes.NewParent.FirstChild.Equal(uRes.Task.ID))
+	a.True(uRes.Task.ID.Equal(t6p1.ID))
+
+	t4p0 = (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t4p0.ID,
+		Parent:  &field.ID{V: p.ID},
+		PrevSib: &field.IDPtr{V: ptr.ID(t1p2.ID)},
+	}).MustDo(ac).Task
+	a.NotNil(t4p0)
+
+	// illegal horizontal move
+	tNil, err = (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t4p0.ID,
+		Parent:  &field.ID{V: p.ID},
+		PrevSib: &field.IDPtr{V: ptr.ID(t4p0.ID)},
+	}).Do(ac)
+	a.Nil(tNil)
+	a.Error(err, "sib loop detected, invalid prevSib value")
+
+	// horizontal move
+	t4p0 = (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t4p0.ID,
+		Parent:  &field.ID{V: p.ID},
+		PrevSib: &field.IDPtr{V: ptr.ID(t1p0.ID)},
+	}).MustDo(ac).Task
+	a.NotNil(t4p0)
+
+	// horizontal move to first position
+	t4p0 = (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t4p0.ID,
+		Parent:  &field.ID{V: p.ID},
+		PrevSib: &field.IDPtr{V: nil},
+		User:    &field.IDPtr{V: ptr.ID(r.Bob().ID())},
+	}).MustDo(ac).Task
+	a.NotNil(t4p0)
+
+	t1p0 = (&task.Update{
+		Host:        r.Ali().ID(),
+		Project:     p.ID,
+		ID:          t1p0.ID,
+		PrevSib:     &field.IDPtr{V: &t1p2.ID},
+		Description: &field.String{V: ""},
+	}).MustDo(ac).Task
+	a.NotNil(t1p0)
+
+	// repeat call should not change anything
+	t1p0 = (&task.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t1p0.ID,
+		PrevSib: &field.IDPtr{V: &t1p2.ID},
+	}).MustDo(ac).Task
+	a.NotNil(t1p0)
+
+	// currently no way of setting this through api as it requires a paywall
+	// which hasnt been implemented yet
+	_, err = r.Data().Primary().Exec(`UPDATE projects SET fileLimit=5000 WHERE id=?`, p.ID)
+	PanicOn(err)
+
+	content1 := []byte("yolo")
+	createFile := &file.Create{
+		Args: &file.CreateArgs{
+			Host:    r.Ali().ID(),
+			Project: p.ID,
+			Task:    t1p0.ID,
+		},
+	}
+	createFile.Name = "yolo.test.txt"
+	createFile.Type = "text/plain"
+	createFile.Size = int64(len(content1))
+	createFile.Content = ioutil.NopCloser(bytes.NewBuffer(content1))
+	f := createFile.MustDo(ac).File
+	a.NotNil(f)
+
+	t2p0 = (&task.Get{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t2p0.ID,
+	}).MustDo(ac)
+
+	deleteParent := (&task.Delete{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t3p0.ID,
+	}).MustDo(ac)
+	a.True(t2p0.ID.Equal(deleteParent.ID))
+	a.Equal(t2p0.ChildN-1, deleteParent.ChildN)
+
+	t1p0Get := (&task.Get{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t1p0.ID,
+	}).MustDo(ac)
+	a.True(t1p0.ID.Equal(t1p0Get.ID))
+
+	t1p1Ancestors := (&task.GetAncestors{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      p.ID,
+	}).MustDo(ac)
+	a.Equal(len(t1p1Ancestors.Set), 0)
+	a.False(t1p1Ancestors.More)
+
+	t1p1Ancestors = (&task.GetAncestors{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t1p1.ID,
+	}).MustDo(ac)
+	a.Equal(t2p0.ID, t1p1Ancestors.Set[0].ID)
+	a.Equal(t1p0.ID, t1p1Ancestors.Set[1].ID)
+	a.Equal(p.ID, t1p1Ancestors.Set[2].ID)
+	a.False(t1p1Ancestors.More)
+
+	t1p1Ancestors = (&task.GetAncestors{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t1p1.ID,
+		Limit:   1,
+	}).MustDo(ac)
+	a.Equal(t2p0.ID, t1p1Ancestors.Set[0].ID)
+	a.True(t1p1Ancestors.More)
+
+	pChildren := (&task.GetChildren{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      p.ID,
+	}).MustDo(ac)
+	a.Equal(t4p0.ID, pChildren.Set[0].ID)
+	a.Equal(t1p2.ID, pChildren.Set[1].ID)
+	a.Equal(t1p0.ID, pChildren.Set[2].ID)
+	a.False(pChildren.More)
+
+	pChildren = (&task.GetChildren{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      p.ID,
+		After:   ptr.ID(t4p0.ID),
+	}).MustDo(ac)
+	a.Equal(t1p2.ID, pChildren.Set[0].ID)
+	a.Equal(t1p0.ID, pChildren.Set[1].ID)
+	a.False(pChildren.More)
+
+	pChildren = (&task.GetChildren{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      p.ID,
+		After:   ptr.ID(t4p0.ID),
+		Limit:   1,
+	}).MustDo(ac)
+	a.Equal(t1p2.ID, pChildren.Set[0].ID)
+	a.True(pChildren.More)
+
+	(&task.Delete{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      t4p0.ID,
+	}).MustDo(ac)
+
+	pID = p.ID
+	tree = (&task.GetTree{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      p.ID,
+	}).MustDo(ac)
 }
--- cmd/trees/pkg/vitem/vitemeps/vitemeps_test.go
+++ cmd/trees/pkg/vitem/vitemeps/vitemeps_test.go
@@ -2,10 +2,346 @@ package vitemeps_test
 
 import (
 	"testing"
+	time_ "time"
 
-	test "github.com/0xor1/tlbx/cmd/trees/pkg/vitem/vitemtest"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/cnsts"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/config"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/project"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/project/projecteps"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/task"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/task/taskeps"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/testutil"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/vitem"
+	"github.com/0xor1/tlbx/cmd/trees/pkg/vitem/vitemeps"
+	. "github.com/0xor1/tlbx/pkg/core"
+	"github.com/0xor1/tlbx/pkg/field"
+	"github.com/0xor1/tlbx/pkg/ptr"
+	"github.com/0xor1/tlbx/pkg/web/app/test"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestEverything(t *testing.T) {
-	test.Everything(t)
+func Everything(t *testing.T) {
+	var (
+		tree task.GetTreeRes
+		pID  ID
+	)
+
+	defer func() {
+		testutil.PrintFullTree(pID, tree)
+	}()
+
+	a := assert.New(t)
+	r := test.NewMeRig(
+		config.Get(),
+		append(append(projecteps.Eps, taskeps.Eps...), vitemeps.Eps...),
+		nil,
+		nil,
+		nil,
+		projecteps.OnDelete,
+		projecteps.OnSetSocials,
+		projecteps.ValidateFCMTopic,
+		true,
+		cnsts.FileBucket)
+	defer r.CleanUp()
+
+	ac := r.Ali().Client()
+
+	p := (&project.Create{
+		Name: "0",
+	}).MustDo(ac)
+	a.NotNil(p)
+
+	defer func() {
+		(&project.Delete{p.ID}).MustDo(ac)
+	}()
+
+	(&project.AddUsers{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Users: []*project.SendUser{
+			{
+				ID:   r.Bob().ID(),
+				Role: cnsts.RoleWriter,
+			},
+		},
+	}).MustDo(ac)
+
+	t1p0 := (&task.Create{
+		Host:        r.Ali().ID(),
+		Project:     p.ID,
+		Parent:      p.ID,
+		PrevSib:     nil,
+		Name:        "1.0",
+		Description: "",
+		IsParallel:  true,
+		User:        ptr.ID(r.Bob().ID()),
+		TimeEst:     100,
+	}).MustDo(ac).Task
+	a.NotNil(t1p0)
+
+	t1Res := (&vitem.Create{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeTime,
+		Est:     ptr.Uint64(45),
+		Inc:     77,
+		Note:    "yolo",
+	}).MustDo(ac)
+	a.NotNil(t1Res)
+	a.True(t1Res.Task.ID.Equal(t1p0.ID))
+	a.True(t1Res.Task.TimeEst == 45)
+	t1 := t1Res.Item
+	a.NotNil(t1)
+
+	t1Res = (&vitem.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeTime,
+		ID:      t1.ID,
+		Inc:     &field.UInt64{V: 33},
+		Note:    &field.String{V: "polo"},
+	}).MustDo(ac)
+	a.NotNil(t1Res)
+	a.True(t1Res.Task.TimeInc == 33)
+	a.True(t1Res.Item.Inc == 33)
+
+	t1 = (&vitem.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeTime,
+		ID:      t1.ID,
+		Inc:     &field.UInt64{V: 44},
+		Note:    &field.String{V: "polo"},
+	}).MustDo(ac).Item
+	a.NotNil(t1)
+
+	tNil := (&vitem.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeTime,
+		ID:      t1.ID,
+	}).MustDo(ac)
+	a.Nil(tNil)
+
+	ts := (&vitem.Get{
+		Host:         r.Ali().ID(),
+		Project:      p.ID,
+		Task:         &t1p0.ID,
+		Type:         vitem.TypeTime,
+		CreatedBy:    ptr.ID(r.Ali().ID()),
+		CreatedOnMin: ptr.Time(Now().Add(-1 * time_.Hour)),
+		CreatedOnMax: ptr.Time(Now()),
+	}).MustDo(ac)
+	a.Equal(t1, ts.Set[0])
+	a.False(ts.More)
+
+	ts = (&vitem.Get{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Type:    vitem.TypeTime,
+		IDs:     IDs{t1.ID},
+	}).MustDo(ac)
+	a.Equal(t1, ts.Set[0])
+	a.False(ts.More)
+
+	t2 := (&vitem.Create{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeTime,
+		Inc:     77,
+		Note:    "solo",
+	}).MustDo(ac).Item
+	a.NotNil(t2)
+
+	ts = (&vitem.Get{
+		Host:         r.Ali().ID(),
+		Project:      p.ID,
+		Type:         vitem.TypeTime,
+		Task:         &t1p0.ID,
+		CreatedBy:    ptr.ID(r.Ali().ID()),
+		CreatedOnMin: ptr.Time(Now().Add(-1 * time_.Hour)),
+		CreatedOnMax: ptr.Time(Now()),
+	}).MustDo(ac)
+	a.Equal(t2, ts.Set[0])
+	a.Equal(t1, ts.Set[1])
+	a.False(ts.More)
+
+	ts = (&vitem.Get{
+		Host:         r.Ali().ID(),
+		Project:      p.ID,
+		Type:         vitem.TypeTime,
+		Task:         &t1p0.ID,
+		CreatedBy:    ptr.ID(r.Ali().ID()),
+		CreatedOnMin: ptr.Time(Now().Add(-1 * time_.Hour)),
+		CreatedOnMax: ptr.Time(Now()),
+		Limit:        1,
+	}).MustDo(ac)
+	a.Equal(t2, ts.Set[0])
+	a.True(ts.More)
+
+	ts = (&vitem.Get{
+		Host:         r.Ali().ID(),
+		Project:      p.ID,
+		Type:         vitem.TypeTime,
+		Task:         &t1p0.ID,
+		CreatedBy:    ptr.ID(r.Ali().ID()),
+		CreatedOnMin: ptr.Time(Now().Add(-1 * time_.Hour)),
+		CreatedOnMax: ptr.Time(Now()),
+		After:        ptr.ID(t2.ID),
+		Limit:        1,
+	}).MustDo(ac)
+	a.Equal(t1, ts.Set[0])
+	a.False(ts.More)
+
+	t1p0 = (&vitem.Delete{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeTime,
+		ID:      t1.ID,
+	}).MustDo(ac)
+	a.True(t1p0.TimeInc == 77)
+	a.True(t1p0.CostInc == 0)
+
+	// costs section
+
+	t1Res = (&vitem.Create{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeCost,
+		Est:     ptr.Uint64(45),
+		Inc:     77,
+		Note:    "yolo",
+	}).MustDo(ac)
+	a.NotNil(t1Res)
+	a.True(t1Res.Task.ID.Equal(t1p0.ID))
+	a.True(t1Res.Task.CostEst == 45)
+	t1 = t1Res.Item
+	a.NotNil(t1)
+
+	t1Res = (&vitem.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeCost,
+		ID:      t1.ID,
+		Inc:     &field.UInt64{V: 33},
+		Note:    &field.String{V: "polo"},
+	}).MustDo(ac)
+	a.NotNil(t1Res)
+	a.True(t1Res.Task.CostInc == 33)
+	a.True(t1Res.Item.Inc == 33)
+
+	t1 = (&vitem.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeCost,
+		ID:      t1.ID,
+		Inc:     &field.UInt64{V: 44},
+		Note:    &field.String{V: "polo"},
+	}).MustDo(ac).Item
+	a.NotNil(t1)
+
+	tNil = (&vitem.Update{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeCost,
+		ID:      t1.ID,
+	}).MustDo(ac)
+	a.Nil(tNil)
+
+	ts = (&vitem.Get{
+		Host:         r.Ali().ID(),
+		Project:      p.ID,
+		Task:         &t1p0.ID,
+		Type:         vitem.TypeCost,
+		CreatedBy:    ptr.ID(r.Ali().ID()),
+		CreatedOnMin: ptr.Time(Now().Add(-1 * time_.Hour)),
+		CreatedOnMax: ptr.Time(Now()),
+	}).MustDo(ac)
+	a.Equal(t1, ts.Set[0])
+	a.False(ts.More)
+
+	ts = (&vitem.Get{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Type:    vitem.TypeCost,
+		IDs:     IDs{t1.ID},
+	}).MustDo(ac)
+	a.Equal(t1, ts.Set[0])
+	a.False(ts.More)
+
+	t2 = (&vitem.Create{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeCost,
+		Inc:     77,
+		Note:    "solo",
+	}).MustDo(ac).Item
+	a.NotNil(t2)
+
+	ts = (&vitem.Get{
+		Host:         r.Ali().ID(),
+		Project:      p.ID,
+		Type:         vitem.TypeCost,
+		Task:         &t1p0.ID,
+		CreatedBy:    ptr.ID(r.Ali().ID()),
+		CreatedOnMin: ptr.Time(Now().Add(-1 * time_.Hour)),
+		CreatedOnMax: ptr.Time(Now()),
+	}).MustDo(ac)
+	a.Equal(t2, ts.Set[0])
+	a.Equal(t1, ts.Set[1])
+	a.False(ts.More)
+
+	ts = (&vitem.Get{
+		Host:         r.Ali().ID(),
+		Project:      p.ID,
+		Type:         vitem.TypeCost,
+		Task:         &t1p0.ID,
+		CreatedBy:    ptr.ID(r.Ali().ID()),
+		CreatedOnMin: ptr.Time(Now().Add(-1 * time_.Hour)),
+		CreatedOnMax: ptr.Time(Now()),
+		Limit:        1,
+	}).MustDo(ac)
+	a.Equal(t2, ts.Set[0])
+	a.True(ts.More)
+
+	ts = (&vitem.Get{
+		Host:         r.Ali().ID(),
+		Project:      p.ID,
+		Type:         vitem.TypeCost,
+		Task:         &t1p0.ID,
+		CreatedBy:    ptr.ID(r.Ali().ID()),
+		CreatedOnMin: ptr.Time(Now().Add(-1 * time_.Hour)),
+		CreatedOnMax: ptr.Time(Now()),
+		After:        ptr.ID(t2.ID),
+		Limit:        1,
+	}).MustDo(ac)
+	a.Equal(t1, ts.Set[0])
+	a.False(ts.More)
+
+	t1p0 = (&vitem.Delete{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		Task:    t1p0.ID,
+		Type:    vitem.TypeCost,
+		ID:      t1.ID,
+	}).MustDo(ac)
+	a.True(t1p0.CostInc == 77)
+
+	pID = p.ID
+	tree = (&task.GetTree{
+		Host:    r.Ali().ID(),
+		Project: p.ID,
+		ID:      p.ID,
+	}).MustDo(ac)
 }
