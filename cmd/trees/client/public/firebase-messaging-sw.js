f811d9233703a7127f34ec178cc8a445f1e8ef36
Daniel Robinson <daniel.robinson.open@gmail.com>
2022-04-19 22:25:19 +0100

    update firebase creds in sw

--- cmd/trees/client/public/firebase-messaging-sw.js
+++ cmd/trees/client/public/firebase-messaging-sw.js
@@ -2,11 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js");
 
 // Initialize Firebase
-firebase.initializeApp({
-  apiKey: "AIzaSyAg43CfgwC2HLC9x582IMq2UwM6NQ3FRCc",
-  projectId: "trees-82a30",
-  messagingSenderId: "69294578877",
-  appId: "1:69294578877:web:1edb203c55b78f43956bd4",
+firebase.initializeApp(
+{
+  apiKey: "AIzaSyDFoddqIYlq5p4gYenOS2o4lgNQV9aO364",
+  projectId: "trees-b8fdf",
+  messagingSenderId: "256753508336",
+  appId: "1:256753508336:web:af836cd97251283faac735"
 });
 const fcm = firebase.messaging();
 
